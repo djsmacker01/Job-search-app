2260327efb8d603540ce2cdb7d6c3e59cb4a49f1
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-02-12 20:35:27 +0000

    modified index.html

--- job-search-frontend/index.js
+++ job-search-frontend/index.js
@@ -4,8 +4,28 @@ const showMessage = document.getElementById('showMessage');
 
 submit.addEventListener('click', async () =>{
 let profileDisplay = document.createElement('div');
+let response = await fetch("http://localhost:3000/jobTitle", {
+ method: 'POST',
+ headers: {'Content-Type': 'application/json'},
+ body: JSON.stringify({jobTitle:employeeInput.value})
+});
+let responseData = await response.json();
+console.log(responseData)
+
+responseData.forEach(job => {
+
+    // Create a paragraph
+    let para = document.createElement("p");
+
+    // put data inside the paragraph
+
+    // Append the paragraph to the div
+//  showMessage.appendChild();
+
+    
+});
 //  profileDisplay.textContent 
- showMessage.appendChild(profileDisplay);
+
  
  console.log('click event occure')
 })
